98a52ed910255b4337058baffc30ae1d109e8be3
stasantipov <antistas@yandex.ru>
2026-04-06 19:55:29 +0300

    Update lesson_4.ts

--- lesson_4.ts
+++ lesson_4.ts
@@ -13,3 +13,6 @@ function cyclicRightShift(bits: number, shift: number) {
 }
 
 cyclicRightShift(0b10000000_00000000_00000000_00000001, 2);
+
+[ДЗ 1](https://github.com/stasantipov/computer-science/blob/main/lesson_1.md)
+[ДЗ 3](https://github.com/stasantipov/computer-science/blob/main/lesson_3.md)
